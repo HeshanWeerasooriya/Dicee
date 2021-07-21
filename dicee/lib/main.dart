4df7b1fca35f04b0277e432bc0ce2ead0706ae1b
Heshan <heshan.imedha@gmail.com>
2021-07-21 16:24:10 +0530

    shakeDices func

--- dicee/lib/main.dart
+++ dicee/lib/main.dart
@@ -13,6 +13,14 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   int leftDiceNumber = 1;
   int rightDiceNumber = 1;
+
+  void shakeDices() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -30,11 +38,7 @@ class _MyAppState extends State<MyApp> {
                 child: Padding(
                   padding: const EdgeInsets.all(16.0),
                   child: TextButton(
-                    onPressed: () {
-                      setState(() {
-                        leftDiceNumber = Random().nextInt(6) + 1;
-                      });
-                    },
+                    onPressed: shakeDices,
                     child: Image.asset(
                       'images/dice$leftDiceNumber.png',
                     ),
@@ -45,11 +49,7 @@ class _MyAppState extends State<MyApp> {
                 child: Padding(
                   padding: const EdgeInsets.all(16.0),
                   child: TextButton(
-                    onPressed: () {
-                      setState(() {
-                        rightDiceNumber = Random().nextInt(6) + 1;
-                      });
-                    },
+                    onPressed: shakeDices,
                     child: Image.asset(
                       'images/dice$rightDiceNumber.png',
                     ),
